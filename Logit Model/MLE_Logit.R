bf4934e66267e753bc98fd800469f83193f0b8a0
Faber993 <f.leone@ilburguniversity.edu>
2019-02-06 11:58:45 +0100

    update - with gradient

--- Logit Model/MLE_Logit.R
+++ Logit Model/MLE_Logit.R
@@ -23,7 +23,20 @@ rep  <- 1000
 logit_obj <- function(beta, y, X) {
              prob <- exp (X%*%beta ) /(1+ exp(X%*%beta ))
              l    <- log(y*prob + (1-y)*(1 - prob))  # log-likelihood
-             return( if((-mean(l))==Inf | is.na(-mean(l))){  10e5 } else{ -mean(l) } ) }
+             v    <- cbind(y*(1 - prob),y*(1 - prob))
+             u    <- cbind((1 -y)* prob,(1 -y)* prob)
+             s    <- v*X - u*X 
+             return( if((-mean(l))==Inf | is.na(-mean(l))){  10e5 } else{ -mean(l) } )}
+
+# Define gradient function
+logit_gr <- function(beta, y, X) {
+             prob <- exp (X%*%beta ) /(1+ exp(X%*%beta ))
+             v    <- cbind(y*(1 - prob),y*(1 - prob))
+             u    <- cbind((1 -y)* prob,(1 -y)* prob)
+             s    <- v*X - u*X 
+             return( -colMeans(s) )}
+
+
 
 # Run simulation
 tic()
@@ -37,7 +50,7 @@ results <- do.call(rbind, mclapply(1:rep, function(i){
   y       <- as.numeric(X%*%beta > epsilon)
   
   # 2. Run optimization 
-  res     <- optim(c(1,-2), logit_obj, gr = NULL, method ="BFGS", hessian=TRUE, X=X, y=y)$par
+  res     <- optim(c(0,0), logit_obj, logit_gr, method ="BFGS", hessian=TRUE, X=X, y=y)$par
   
 }))
 
@@ -51,5 +64,4 @@ toc()
 
 ## Problems of the code:
 # 1. very sensitive to initial values -> check for a better solver
-# 2. cannot introduce gradient function
 
