abf4c85555bc901730343e975e1b233ba23e391b
Faber993 <f.leone@ilburguniversity.edu>
2019-02-07 10:59:46 +0100

    update

--- Logit Model/MLE_Logit.jl
+++ Logit Model/MLE_Logit.jl
@@ -1,6 +1,7 @@
-# Monte Carlo Simulation - Probit Model
+# Monte Carlo Simulation - Logit Model
 # Fabrizio Leone
 # 07 - 02 - 2019
+
 
 # Import Packages
 
@@ -23,19 +24,16 @@ N   = 1000;
 b   = [0.2, -0.1];
 rep = 1000;
 
-
-# Define probit objective function
-
-function probit_obj(b::Array{Float64,1},y::Array{Int64,2},X::Array{Float64,2},N::Int64)
-    d    = Normal()
-    pcdf = cdf.(d, X*b)
-    ppdf = pdf.(d, X*b)
-    l    = y.*log.(pcdf) + (ones(N,1)-y).*log.(ones(N,1)-pcdf)    # Likelihood
-    s    = ppdf.*X.*(y-pcdf)./(pcdf.*(ones(N,1)-pcdf))            # Gradient
+# Define logit objective function
+function logit_obj(b::Array{Float64,1},y::Array{Int64,2},X::Array{Float64,2},N::Int64)
+    prob = exp.(X*b) ./ (ones(N,1)+ exp.(X*b))
+    l    = log.(y.*prob + (ones(N,1)-y).*(ones(N,1)-prob))        # Likelihood
+    s    = y.*(ones(N,1) - prob).*X -((ones(N,1) -y).* prob .*X)  # Gradient
     nll  = -mean(l)
     ns   = -sum!([1. 1.], s)/N
     return nll, ns
 end
+
 
 # Run Monte Carlo Simulation
 
@@ -48,16 +46,16 @@ for i = 1:rep
 # 1. Simulate Data
 c    = ones(N,1);
 X    = hcat(c, rand(Chisq(10),N,1));
-ϵ    = rand(Normal(0,1),N,1);
+ϵ    = -rand(Logistic(0,1),N,1);
 y    = Int.(X*b.>ϵ);
 
 # 2. Run optimization
 function objfun(x)
-        probit_obj([x[1],x[2]],y,X,N)[1]
+        logit_obj([x[1],x[2]],y,X,N)[1]
 end
 
 function gradfun!(G,x)
-        grad = probit_obj([x[1],x[2]],y,X,N)[2]
+        grad = logit_obj([x[1],x[2]],y,X,N)[2]
         G[1] = grad[1]
         G[2] = grad[2]
 end
