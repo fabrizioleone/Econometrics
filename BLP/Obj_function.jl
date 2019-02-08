d1d835bca170a6e9cbf083d385e46e6bfbff7034
Faber993 <f.leone@ilburguniversity.edu>
2019-02-08 11:22:47 +0100

    Update Obj_function.jl

--- BLP/Obj_function.jl
+++ BLP/Obj_function.jl
@@ -12,12 +12,23 @@ delta      = X*theta1;
 
 while norm_max > tol_inner  && ii < 1000
 
-     # step 1: simulated market share
-     num       = delta.*exp.([A price]*(theta2.*v));
-     den       = ones(TM,1).+sharesum*num;
-     den       = sharesum'*den;
-     sim_share = (1/nn)*sum(num./den,2);
+     # Step 1: Simulated market shares
+     num       = delta.*exp.([A price]*(theta2.*v)); # Numerator of simulated integral
+     den       = ones(TM,1).+sharesum*num;           # Denominator of simulated integral
+     den       = sharesum'*den;                      # Denominator of simulated integral
+     sim_share = sum(num./den,dims=2);               # Simulated shares
 
+     # Step 2: Compute a new delta by BLP inversion and compute norm_max
+     delta_new = delta.*(share./sim_share);          # BLP contraction mapping
+     norm_max  = maximum(abs.(delta_new - delta));   # Find maximum of Euclidean distance
+     delta     = delta_new;                          # Update delta
+     global i += 1                                   # Update counter
 
-global i += 1
 end
+
+     # Step 3: Get the implied structural errors
+     xi        = log(delta_new) - X*theta1;          # Updated moment condition
+     g         = Z'*xi;                              # Moment conditions GMM
+
+     # Step 4: Update GMM objective function
+     f         = g'*W*g;      
