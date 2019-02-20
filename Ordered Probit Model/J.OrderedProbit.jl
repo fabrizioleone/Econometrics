14e3d1c505211a8c859238d6e70e3aa9aa07574c
Faber993 <f.leone@ilburguniversity.edu>
2019-02-20 23:47:12 +0100

    Update J.OrderedProbit.jl

--- Ordered Probit Model/J.OrderedProbit.jl
+++ Ordered Probit Model/J.OrderedProbit.jl
@@ -27,13 +27,13 @@ startvalues = rand(Normal(0,1),Npar,1);                                         
 repetitions = 1000;
 
 # Define ordered probit objective function
-function nll_OrderedProbit(pars::Array{Float64,1}, y::Array{Int64,2}, x::Array{Int64,2})
-    thresholds  = [-Inf pars[3] pars[4] Inf];
-    Xb          = x[:,1].*pars[1] + x[:,2].*pars[2];
-    p           = cdf.(Normal(0,1),thresholds[y.+1] - Xb) - cdf.(Normal(0,1),thresholds[y] - Xb);
-    nll         = - mean(log.(p));
-    return nll
-end
+#function nll_OrderedProbit(pars::Array{Float64,1}, y::Array{Int64,2}, x::Array{Int64,2})
+#    thresholds  = [-Inf pars[3] pars[4] Inf];
+#    Xb          = x[:,1].*pars[1] + x[:,2].*pars[2];
+#    p           = cdf.(Normal(0,1),thresholds[y.+1] - Xb) - cdf.(Normal(0,1),thresholds[y] - Xb);
+#    nll         = - mean(log.(p));
+#    return nll
+#end
 
 # Run Monte Carlo Simulation
 beta_hat = zeros(N,Npar)
@@ -49,8 +49,39 @@ ystar     = x[:,1].*beta[1] + x[:,2].*beta[2] + ϵ
 y         = Int.(1 .+ (ystar.>alpha[1]) + (ystar.>alpha[2]));
 
 # 2. Run optimization
-function objfun(b)
-        nll_OrderedProbit(b[1:4],y,x)
+#function objfun(b)
+#        nll_OrderedProbit(b[1:4],y,x)
+#end
+
+
+
+function fg!(F, G, pars)
+    thresholds  = [-Inf pars[3] pars[4] Inf];
+    Xb          = x[:,1].*pars[1] + x[:,2].*pars[2];
+    p           = cdf.(Normal(0,1),thresholds[y.+1] - Xb) - cdf.(Normal(0,1),thresholds[y] - Xb);
+
+
+    if !(G==nothing)
+    dLdbeta1    = (y==1).*(cdf.(Normal(0,1), pars(3) - Xb).*(-x[:,1]))./p...
+                + (y==2).*(cdf.(Normal(0,1), pars(4) - Xb) - cdf.(Normal(0,1), pars(3) - Xb)).*(-x[:,1])./p...
+                + (y==3).*cdf.(Normal(0,1), pars(4) - Xb).*x[:,1]./p;
+
+   dLdbeta1     = (y==1).*(cdf.(Normal(0,1), pars(3) - Xb).*(-x[:,2]))./p...
+                + (y==2).*(cdf.(Normal(0,1), pars(4) - Xb) - cdf.(Normal(0,1), pars(3) - Xb)).*(-x[:,2])./p...
+                + (y==3).*cdf.(Normal(0,1), pars(4) - Xb).*x[:,2]./p;
+
+    dLdalpha1   = (y==1).*normpdf(pars(3) - Xb)./p...
+                + (y==2).*normpdf(pars(3) - Xb).*(-1)./p;
+
+    dLdalpha2   = (y==2).*normpdf(pars(4) - Xb)./p...
+                + (y==3).*normpdf(pars(4) - Xb).*(-1)./p;
+
+
+    gradient    = [dLdbeta1, dLdbeta2, dLdalpha1, dLdalpha2];
+
+    ns          = - mean(gradient);
+    end
+
 end
 
 
@@ -63,4 +94,9 @@ end
 
 # Show and Plot results
 println("Mean Estimates ",sum!([1. 1. 1. 1.], beta_hat)/repetitions)
-println("SE Estimates ", std(beta_hat; mean=nothing, dims=1)/sqrt(N))
+println("SE Estimates ", std(beta_hat; mean=nothing, dims=1))
+h1 = histogram(beta_hat[:,1], label = "beta_1")
+h2 = histogram(beta_hat[:,2], label = "beta_2")
+h3 = histogram(beta_hat[:,3], label = "alpha_1")
+h4 = histogram(beta_hat[:,4], label = "alpha_2")
+plot(h1,h2,h3,h4, layout=(2,2),legend=false)
