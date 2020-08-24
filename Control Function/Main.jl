b15a34273e8d1bc5b8e9805c1eb4eb086801f162
Fab993 <44437403+Fab993@users.noreply.github.com>
2020-08-24 11:59:05 +0200

    added control function approach for PPML with fixed effects

--- Control Function/Main.jl
+++ Control Function/Main.jl
@@ -11,7 +11,7 @@
 #Pkg.add("LinearAlgebra")
 #Pkg.add("Distributed")
 #Pkg.add("Plots")
-using CSV, DataFrames, FixedEffectModels, Random, LinearAlgebra, Distributions, Distributed, Base.Threads, Plots
+using CSV, DataFrames, GLM, GLFixedEffectModels, FixedEffectModels, Random, LinearAlgebra, Distributions, Distributed, Base.Threads, Plots
 Random.seed!(1320);
 rng = MersenneTwister(1320)
 
@@ -32,15 +32,19 @@ end
 function Create_data(par,ctr)
     ID   = sum(kron(1:ctr.N,Matrix{Int64}(I,ctr.T,ctr.T)),dims=2)                    # Panel ID: individual
     Time = sum(kron(Matrix{Int64}(I,ctr.N,ctr.N),1:ctr.T),dims=2)                    # Panel ID: time
-    aID  = sum(kron(rand(Normal(6,1),ctr.N,1),Matrix{Int64}(I,ctr.T,ctr.T)),dims=2)  # individual FE
-    aT   = sum(kron(Matrix{Int64}(I,ctr.N,ctr.N),rand(Normal(5,10),ctr.T,1)),dims=2) # time FE
+    aID  = sum(kron(rand(Normal(0,1),ctr.N,1),Matrix{Int64}(I,ctr.T,ctr.T)),dims=2)  # individual FE
     v    = rand(Normal(0,1),ctr.N*ctr.T,1)                                           # endogenous part of X
     u    = ctr.rho*v .+ rand(Normal(0,1),ctr.N*ctr.T,1)                              # error main regressions
-    Z    = exp.(rand(Normal(0,1),ctr.N*ctr.T,1))                                     # Instrument for X
-    X    = aID .+ aT .+ par.gamma1*Z .+ v                                            # endogenous regressor
-    y    = aID .+ aT .+ X * par.beta1  .+ u                                          # outcome
-    df   = DataFrame([ID Time y X Z])
-    colnames = ["ID","Time","y","X","Z"]
+    Z    = rand(Chisq(3),ctr.N*ctr.T,1)                                              # Instrument for X
+    X    = aID .+ par.gamma1*Z .+ v                                                  # endogenous regressor
+    y    = aID .+ X * par.beta1  .+ u                                                # outcome linear
+    y1   = ones(ctr.T*ctr.N,1) .* NaN                                                # initialize outcome Poisson
+    lbd  = exp.(aID  .+ X * par.beta1)                                               # mean Poisson
+    for j = 1:ctr.T*ctr.N 
+        y1[j] = rand(Poisson(lbd[j]))                                                # fill outcome Poisson
+    end                         
+    df   = DataFrame([ID Time y y1 X Z])
+    colnames = ["ID","Time","y", "y1", "X","Z"]
     rename!(df, colnames)
 
     return df
@@ -52,32 +56,39 @@ function MonteCarlo(par,ctr)
     df = Create_data(par,ctr)
 
     # 1.Endogenous Regression
-    out1 = reg(df, @formula(y ~ X + fe(ID) + fe(Time))).coef[1]
+    out1 = reg(df, @formula(y ~ X + FixedEffectModels.fe(ID))).coef[1]
 
     # 2. Control Function Approach
-    df[!, :CF] = StatsBase.residuals(reg(df, @formula(X ~ Z + fe(ID) + fe(Time)), save = true))
-    out2 = reg(df, @formula(y ~ X + CF + fe(ID) + fe(Time))).coef[1]
+    df[!, :CF] = reg(df, @formula(X ~ Z + FixedEffectModels.fe(ID)), save = true).residuals
+    out2 = reg(df, @formula(y ~ X + CF + FixedEffectModels.fe(ID))).coef[1]                                        # Linear regression
+    out3 = nlreg(df, @formula(y1 ~ X + GLFixedEffectModels.fe(ID)), Poisson(), LogLink(), start = [0.8]).coef[1]   # Poisson regression
 
-    return [out1 out2]
+    return [out1 out2 out3]
 end
 
 # Initialize function to run the simulation
 function MC_execute(par,ctr)
-    out = zeros(ctr.Boot,2)
+    out = zeros(ctr.Boot,3)
         @threads for i = 1:ctr.Boot
-                    out[i,:] = MonteCarlo(par,ctr)
-                 end
+            try
+                out[i,:] = MonteCarlo(par,ctr)
+            catch
+                skip                      # Skip if nlreg fails to converge (this may happen if using another starting value than 0.8)
+            end
+        end
 
     return out
 end
 
-#Run simulation
-par = Params(1.0, 2.0)
-ctr = Ctr(100, 5, 1.5, 1000)
+# Run simulation
+par = Params(0.5, 1.0);
+ctr = Ctr(100, 5, 1.5, 1000);
 @time MCout = MC_execute(par,ctr)
-@show mean(MCout,dims=1) std(MCout,dims=1)
+@show mean(MCout, dims = 1) std(MCout, dims = 1)
 
 # Plot results
-h1 = histogram(MCout[:,1])
-h2 = histogram(MCout[:,2])
-plot(h1,h2,layout=(1,2),legend=false)
+h1 = histogram(MCout[:,1]);
+h2 = histogram(MCout[:,2]);
+h3 = histogram(MCout[:,3]);
+plot(h1,h2,h3, layout=(1,3), legend=false)
+
